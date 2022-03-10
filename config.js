d3c2d040b9c52150aac8512f63d00f73c02b8c25
lukejandrews <87306715+lukejandrews@users.noreply.github.com>
2022-03-10 15:16:05 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,27 +6,13 @@ const config = {
     trailingSlash: false,
   },
   header: {
-    logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
-    logoLink: 'https://hasura.io/learn/',
-    title:
-      "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
-    githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
+    logo: 'https://www.10xbanking.com/hubfs/Logos/10xbanking-logo-1.svg',
+    logoLink: '',
+    title: 'Client portal' ,
+    githubUrl: 'https://github.com/lukejandrews/portal-',
     helpUrl: '',
     tweetText: '',
-    social: `<li>
-		    <a href="https://twitter.com/hasurahq" target="_blank" rel="noopener">
-		      <div class="twitterBtn">
-		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Twitter'}/>
-		      </div>
-		    </a>
-		  </li>
-			<li>
-		    <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
-		      <div class="discordBtn">
-		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
-		      </div>
-		    </a>
-		  </li>`,
+    social: '',
     links: [{ text: '', link: '' }],
     search: {
       enabled: false,
@@ -52,10 +38,10 @@ const config = {
   },
   siteMetadata: {
     title: 'Gatsby Gitbook Boilerplate | Hasura',
-    description: 'Documentation built with mdx. Powering hasura.io/learn ',
+    description: 'Documentation built with mdx.',
     ogImage: null,
     docsLocation: 'https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content',
-    favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
+    favicon: 'https://www.10xbanking.com/hubfs/Logos/10xbanking-logo-1.svg',
   },
   pwa: {
     enabled: false, // disabling this will also remove the existing service worker.
